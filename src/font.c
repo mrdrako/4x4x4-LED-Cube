1a334e6b4cada2953aaf502216c72eeec0fffed2
mrdrako <mrdrako87@yahoo.com>
2013-09-20 19:20:14 -0400

    initialized stub font file pattern

---
+++ src/font.c
@@ -0,0 +1,22 @@
+// ############################################
+// 
+// 4x4x4 LED Cube project
+// By Christian Moen 2008
+// chr@syntaks.org
+// License: GPL
+//
+// Additional material
+// by Mike Durakovich
+// mrdrako87@yahoo.com
+// license: GPL
+//
+// ############################################
+
+// Text library for 4x5 vertically scrolling font
+
+const unsigned char font[2][4] PROGMEM =                //94 counting lowercase, upper, nums, and all punct
+{
+	{ 0x0f, 0x13, 0x13, 0x0f },        // A
+	{ 0x1f, 0x15, 0x15, 0x10 },        // B
+};
+
